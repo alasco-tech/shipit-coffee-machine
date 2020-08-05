ce61649ecd1486e201596e96b9415e1cd29231aa
Christian Ittner <christian.ittner@alasco.de>
2020-08-05 14:43:31 +0200

    add picture.py

---
+++ picture.py
@@ -0,0 +1,16 @@
+import picamera as _picamera
+import datetime as _dt
+
+PICTURE_FOLDER = "/home/pi/pictures/"
+camera = _picamera.PiCamera()
+# camera.rotation = 180
+
+def take_picture() -> str:
+    """ takes a picture, stores it locally and returns the filepath """
+    filename = PICTURE_FOLDER + str(_dt.datetime.now()) + ".jpg"
+    camera.capture(filename)
+    return filename
+
+
+if __name__ == "__main__":
+    take_picture()
