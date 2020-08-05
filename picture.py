41a1d46a4a2d5f3798fa4c496c02bfc1f9469f48
Christian Ittner <christian.ittner@alasco.de>
2020-08-05 14:56:01 +0200

    put pieces together

--- picture.py
+++ picture.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python3
-import picamera as _picamera
 import datetime as _dt
+import os as _os
+import picamera as _picamera
 
 PICTURE_FOLDER = "/home/pi/pictures/"
+
+if not _os.path.exists(PICTURE_FOLDER):
+    _os.makedirs(PICTURE_FOLDER)
+
 camera = _picamera.PiCamera()
 # camera.rotation = 180
 
